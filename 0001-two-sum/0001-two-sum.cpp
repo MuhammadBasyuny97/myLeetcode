27b45c4629fd319833b112b3fbff3460397a0920
MuhammadBasyuny97 <101678105+MuhammadBasyuny97@users.noreply.github.com>
2023-03-13 06:22:55 +0200

    Time: 11 ms (84.89%), Space: 11.1 MB (17.67%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,20 +2,21 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
           map<int,int> m;
-          vector<int> indices;
+          //vector<int> indices;
         for(int i {0}; i < nums.size(); ++i){
             int diff = target - nums[i];
             if(m.find(diff) != m.end()){
-                indices.push_back(m[diff]);
-                indices.push_back(i);
-                break;
+              //  indices.push_back(m[diff]);
+               // indices.push_back(i);
+               // break;
+                return {m[diff] , i};
             }
            else{
                m[nums[i]] = i;
                //m.insert(make_pair(nums[i] , i));
            } 
         }
-          return indices;     
+          return {};     
         }
         
     
