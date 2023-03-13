cfc1a9e181ef22804a9722986f47f4fbc530b36d
MuhammadBasyuny97 <101678105+MuhammadBasyuny97@users.noreply.github.com>
2023-03-13 08:46:11 +0200

    Time: 6 ms (83.81%), Space: 7.4 MB (17.47%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,10 +2,17 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
        if(s.size() != t.size()) return false;
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-       
+       if(s == t) return true;
+        
+        unordered_map<char , int> umap ;
       
-        return s == t;
+        for(int i{0}; i < s.size(); ++i){
+            umap[s[i]]++;
+            umap[t[i]]--;
+        }
+        for(auto c: umap){
+            if(c.second) return false;
+        }
+        return true;
     }
 };
