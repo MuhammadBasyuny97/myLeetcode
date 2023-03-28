4df2adc1faf058c8aae67c99c6e290dd7432ecf0
MuhammadBasyuny97 <101678105+MuhammadBasyuny97@users.noreply.github.com>
2023-03-28 17:03:56 +0200

    Time: 39 ms (61.77%), Space: 20.8 MB (47.26%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        map<string , vector<string>> m;
+        unordered_map<string , vector<string>> m;
         vector<vector<string>> groups;
         for(auto str: strs){
             string str_copy = str;
             sort(str_copy.begin(), str_copy.end());
+            
             if(m.find(str_copy) != m.end()){
                 m[str_copy].push_back(str);
-           }
+             }
             else{
                 m[str_copy] = {str};
             }
-        }
+         }
+        
         for(auto key : m){
             groups.push_back(key.second);
         }
