989233695a72995033e719a09c05f2fd94b044df
MuhammadBasyuny97 <101678105+MuhammadBasyuny97@users.noreply.github.com>
2023-05-29 17:48:00 +0300

    Time: 4 ms (36.85%), Space: 6.7 MB (69.05%) - LeetHub

--- 1446-consecutive-characters/1446-consecutive-characters.cpp
+++ 1446-consecutive-characters/1446-consecutive-characters.cpp
@@ -1,21 +1,13 @@
 class Solution {
 public:
     int maxPower(string s) {
-        int i {0} , j {0};
-        int n = s.length();
-        char check;
-        int power{1};
-        
-        while (j < n){
-            if(s[j] != check){
-                 i = j ;
-           }else{
-                power = max(power,j - i + 1) ; 
-                }
-            
-               check = s[j];
-                   ++j;
+       int i=0,j=0,ans=0,ln=s.size();
+        while(j<ln){
+            while(j<ln && s[j]==s[i])j++;
+            ans= max(ans, j-i);
+            i=j;
         }
-        return power;
+        return ans;
     }
+    
 };
