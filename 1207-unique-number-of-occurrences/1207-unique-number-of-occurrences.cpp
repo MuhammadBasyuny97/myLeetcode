d3a5ae556069e3bfb36af74c38b8d0f16d1423f5
MuhammadBasyuny97 <101678105+MuhammadBasyuny97@users.noreply.github.com>
2023-08-02 06:04:52 +0300

    Time: 5 ms (30.74%), Space: 8.2 MB (67.02%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        map<int,int> m;
+        unordered_map<int,int> m;
         for(const auto &elem: arr){
             m[elem]++;
         }
