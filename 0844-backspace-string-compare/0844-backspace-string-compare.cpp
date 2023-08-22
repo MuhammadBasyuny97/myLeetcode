7396b0f6a784649dc1de695307e3b8f4ec4e3035
MuhammadBasyuny97 <101678105+MuhammadBasyuny97@users.noreply.github.com>
2023-08-22 08:45:34 +0300

    Time: 3 ms (44.98%), Space: 6.3 MB (70.60%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.cpp
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    
-     string deleteBackspace(string s){
+    //Solution 1
+    /* string deleteBackspace(string s){
         int n = s.length();
         int count = 0;
         string new_str;
@@ -22,5 +22,47 @@ public:
         s = deleteBackspace(s);
         t = deleteBackspace(t);
         return s == t;
+    }*/
+    
+    //======== Solution 2 ============
+    bool backspaceCompare(string s, string t){
+        int i = s.length() - 1;
+        int j = t.length() - 1;
+        
+        int count_s = 0;
+        int count_t = 0;
+        while(i >= 0 || j >= 0){
+            while(i >= 0){
+                if(s[i] == '#'){
+                    ++count_s;
+                    --i;
+                }
+                else if (count_s > 0){
+                    --count_s;
+                    --i;
+                }
+                else{
+                    break;
+                }
+            }
+             while(j >= 0){
+                if(t[j] == '#'){
+                    ++count_t;
+                    --j;
+                }
+                else if (count_t > 0){
+                    --count_t;
+                    --j;
+                }
+                else{
+                    break;
+                }
+            }
+            if(i >= 0 && j >= 0 && s[i] != t[j]) return false;
+            if((i >= 0) != (j >= 0)) return false;
+           
+           --i; --j;
+        }
+        return true;
     }
 };
