42f6a35a180389b27c0aec84346e839a1e0d3989
MuhammadBasyuny97 <101678105+MuhammadBasyuny97@users.noreply.github.com>
2023-08-24 09:06:50 +0300

    Time: 42 ms (32.50%), Space: 12.6 MB (49.42%) - LeetHub

--- 2716-minimize-string-length/2716-minimize-string-length.cpp
+++ 2716-minimize-string-length/2716-minimize-string-length.cpp
@@ -1,13 +1,19 @@
 class Solution {
 public:
-    int minimizedStringLength(string s) {
-        set<char> set;
+   int minimizedStringLength(string s) {
+      //======= Solution 1 ==========
+     /*   set<char> set;
         int min_s{0};
         for(int i{0}; i < s.size() ; ++i){
             if(set.find(s[i]) == set.end()){
                 set.insert(s[i]);
             }
         }
-        return set.size();
+        return set.size();*/
+       
+       //======Solution 2=======
+       unordered_set<char>st;
+       for(auto c: s) st.insert(c);
+       return st.size();
     }
 };
